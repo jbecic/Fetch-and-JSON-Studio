516467f941578482a56dc83d0671412055b1d764
Jasmin Becic <jasminbecic@Jasmins-MacBook-Air.local>
2021-11-21 21:46:45 -0600

    completed main studio, need to work on bonus portion

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 // TODO: add code here
+function skillsToString(array) {
+    let str;
+    for (let i = 0; i < array.length; i++) {
+        if (i === 0) {
+            str = array[i] + ', ';
+        } else if (i + 1 === array.length) {
+            str +=  array[i];
+        } else {
+            str += array[i] + ', ';
+        }
+    }
+
+    return str;
+}
+
+function init() {
+    const fetchPromise = fetch('https://handlers.education.launchcode.org/static/astronauts.json');
+    fetchPromise.then(function(response) {
+        const jsonPromise = response.json();
+        jsonPromise.then(function(json) {
+            console.log(json);
+            const container = document.getElementById('container');
+            for (let i = 0; i < json.length; i++) {
+                container.innerHTML += `
+                    <div class='astronaut'>
+                        <div class='bio'>
+                            <h3>${json[i].firstName + ' ' + json[i].lastName}</h3>
+                            <ul>
+                                <li>Hours in space: ${json[i].hoursInSpace}</li>
+                                <li>Active: ${json[i].active}</li>
+                                <li>Skills: ${skillsToString(json[i].skills)} </li>
+                            </ul>
+                        </div>
+                        <img class='avatar' src='${json[i].picture}'>
+                    </div>
+                `;
+            }
+        });
+    });
+};
+
+window.addEventListener('load', init);
